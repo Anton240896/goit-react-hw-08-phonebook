66dbc8e8288ac7407f6ee23c654439d34f4d2380
Anton240896 <antonalipov240811111111@gmail.com>
2023-11-28 10:01:00 +0000

    Deploying to gh-pages from @ Anton240896/goit-react-hw-08-phonebook@c3ff1d6a8a5965cf9296766207d0bfb517f14155 🚀

--- static/js/304.5c18eb1b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[304],{304:function(n,e,o){o.r(e),o.d(e,{default:function(){return h}});var t,r=o(168),i=o(5867).ZP.h1(t||(t=(0,r.Z)(["\n  margin-left: 916px;\n  margin-top: -221px;\n  font-size: 30px;\n"]))),c=o(184);function h(){return(0,c.jsx)("div",{children:(0,c.jsx)(i,{children:(0,c.jsx)("p",{children:"Welcome to your virtual phone \ud83d\udcf1!"})})})}}}]);
-//# sourceMappingURL=304.5c18eb1b.chunk.js.map
